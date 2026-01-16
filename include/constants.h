8925e2c8d2d4592d059ed5da3b320b3ad309e204
ximenapatriciagarcia <ximenascoding@gmail.com>
2026-01-15 21:26:21 -0600

    Add Omnidirectional Kinematics directory/class

---
+++ include/constants.h
@@ -0,0 +1,20 @@
+/**
+ * @file constants.h
+ * @date 12/01/2026
+ * @author Ximena Patricia García Magdaleno
+ *
+ * @brief Constants for the robot.
+ */
+
+#ifndef CONSTANTS_H
+#define CONSTANTS_H
+
+#include <math.h>
+
+namespace Constants
+{
+    
+}
+
+#endif
+
