0f05f732119a2575f1731236e719c69853e59789
ximenapatriciagarcia <ximenascoding@gmail.com>
2026-02-07 22:17:37 -0600

    Changed variables to constants

--- lib/OmniKinematics/omni_motors.cpp
+++ lib/OmniKinematics/omni_motors.cpp
@@ -8,29 +8,30 @@
  * @date 2026-01-12
  */
 
-
-//#include  "constants.h"
 #include "omni_motors.hpp"
+#include "constants.h"
+
 #include <math.h>
 
 
-static constexpr float DEG2RAD = PI / 180.0f;
 
-static constexpr float M1_ANGLE = 135.0f;  //M1
-static constexpr float M2_ANGLE = 45.0f;   //M2
-static constexpr float M3_ANGLE = -45.0f;  //M3
-static constexpr float M4_ANGLE = -135.0f; //M4
+static constexpr float DEG2RAD = Constants::DriveConstants::kDEG2RAD;
+
+static constexpr float M1_ANGLE = Constants::Kinematics::M1_ANGLE;  //M1
+static constexpr float M2_ANGLE = Constants::Kinematics::M2_ANGLE;  //M2
+static constexpr float M3_ANGLE = Constants::Kinematics::M3_ANGLE;  //M3
+static constexpr float M4_ANGLE = Constants::Kinematics::M4_ANGLE;  //M4
 
 
 void OmniMotors::normalize4(float &a, float &b, float &c, float &d) 
 {
 // NORMALIZAR llantas en conjunto
-  float m = fabsf(a);
-  m = fmaxf(m, fabsf(b));
-  m = fmaxf(m, fabsf(c));
-  m = fmaxf(m, fabsf(d));
+  float m = fabsf(a); 
+  m = fmaxf(m, fabsf(b)); 
+  m = fmaxf(m, fabsf(c)); 
+  m = fmaxf(m, fabsf(d)); 
 
-  if (m > 1.0f) { a/=m; b/=m; c/=m; d/=m; }
+  if (m > 1.0f) { a/=m; b/=m; c/=m; d/=m; } 
 }
 
 
@@ -50,9 +51,9 @@ void OmniMotors::setWheelCmd(DCMotor& m, float cmd) {
 
 void OmniMotors::MoveXYW(float vx, float vy, float omega)
 {
-  //calculo de velocidades + control con PID
+  
   float m1 = vx * cosf(M1_ANGLE * DEG2RAD)
-           + vy * sinf(M1_ANGLE * DEG2RAD) // tal vez cambie
+           + vy * sinf(M1_ANGLE * DEG2RAD) 
             + omega;
 
            
@@ -79,84 +80,4 @@ void OmniMotors::MoveXYW(float vx, float vy, float omega)
   setWheelCmd(lower_left_,  m4);
 }
 
-
-//=====POR SI NO FUNICONA LA FUNION ORIGINAL=========
-/* 
-// Funcion para cuando no se tiene PID
-void Motors::MoveMotors(int degree, uint8_t speed)
-{
-    float m1 = cos(((45 + degree) * PI / 180));
-    float m2 = cos(((135 + degree) * PI / 180));
-    float m3 = cos(((225 + degree) * PI / 180));
-    float m4 = cos(((315 + degree) * PI / 180));
-    int speedA = abs(int(m1 * speed));
-    int speedB = abs(int(m2 * speed));
-    int speedC = abs(int(m3 * speed));
-    int speedD = abs(int(m4 * speed));
-
-    analogWrite(motor1.GetSpeed(), speedA);
-    analogWrite(motor2.GetSpeed(), speedB);
-    analogWrite(motor3.GetSpeed(), speedC);
-    analogWrite(motor4.GetSpeed(), speedD);
-
-    if (m1 >= 0)
-    {
-        motor1.MoveForward();
-    }
-    else
-    {
-        motor1.MoveBackward();
-    }
-    if (m2 >= 0)
-    {
-        motor2.MoveForward();
-    }
-    else
-    {
-        motor2.MoveBackward();
-    }
-    if (m3 >= 0)
-    {
-        motor3.MoveForward();
-    }
-    else
-    {
-        motor3.MoveBackward();
-    }
-    if (m4 >= 0)
-    {
-        motor4.MoveForward();
-    }
-    else
-    {
-        motor4.MoveBackward();
-    }
-}
-*/
-
-
-/*
-void MoveMotors(float degree, floatt speed, float omega){
-
-    float m2 = cos(((45 + degree) * PI / 180)) * speed + speed_w;
-    float m3 = cos(((135 + degree) * PI / 180)) * speed + speed_w;
-    float m4 = cos(((225 + degree) * PI / 180)) * speed + speed_w;
-    float m1 = cos(((315 + degree) * PI / 180)) * speed + speed_w;
-    int speedA = abs(int(m1));
-    int speedB = abs(int(m2));
-    int speedC = abs(int(m3));
-    int speedD = abs(int(m4));
-
-    Serial.print("m1: "); Serial.print(m1); Serial.print(", speedA: "); Serial.println(speedA);
-    Serial.print("m2: "); Serial.print(m2); Serial.print(", speedB: "); Serial.println(speedB);
-    Serial.print("m3: "); Serial.print(m3); Serial.print(", speedC: "); Serial.println(speedC);
-    Serial.print("m4: "); Serial.print(m4); Serial.print(", speedD: "); Serial.println(speedD);
-
-
-    analogWrite(motor1.GetSpeed(), speedA);
-    analogWrite(motor2.GetSpeed(), speedB);
-    analogWrite(motor3.GetSpeed(), speedC);
-    analogWrite(motor4.GetSpeed(), speedD);
-}
-*/
 
