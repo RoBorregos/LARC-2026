94b320c909b19427b8ce8bf62717343772e7a47d
ximenapatriciagarcia <ximenascoding@gmail.com>
2026-02-18 14:48:28 -0600

    The file constants.h was formatted

--- include/constants.h
+++ include/constants.h
@@ -5,14 +5,6 @@
  *
  * @brief Constants for the robot.
  */
-/**
- * @file constants.h
- * @date 12/01/2026
- * @author Ximena Patricia García Magdaleno
- *
- * @brief Constants for the robot.
- */
-
 
 #ifndef CONSTANTS_H
 #define CONSTANTS_H
@@ -20,45 +12,38 @@
 #include <Arduino.h>
 #include <math.h>
 
-
-
 namespace Constants
 {
-    namespace SystemConstants
-    {
-        constexpr float kUpdateInterval = 20.0;
-    }
+namespace SystemConstants
+{
+constexpr float kUpdateInterval = 20.0;
+} // namespace SystemConstants
 
+namespace Kinematics
+{
+// "omni_motors" class
+constexpr float M1_ANGLE = 135.0f;  //M1
+constexpr float M2_ANGLE = 45.0f;   //M2
+constexpr float M3_ANGLE = -45.0f;  //M3
+constexpr float M4_ANGLE = -135.0f; //M4
+} // namespace Kinematics
 
-    namespace Kinematics
-    {
-        // "omni_motors" class
-    constexpr float M1_ANGLE = 135.0f;  //M1
-    constexpr float M2_ANGLE = 45.0f;   //M2
-    constexpr float M3_ANGLE = -45.0f;  //M3
-    constexpr float M4_ANGLE = -135.0f; //M4
-    
-    }
-    namespace DriveConstants
-    {
-        // "omni_motors" class
-    static constexpr float kDEG2RAD = PI / 180.0f;
+namespace DriveConstants
+{
+static constexpr float kDEG2RAD = PI / 180.0f;
 
-    constexpr float kWheelDiameter = 0.109f;
-    constexpr float kWheelRaius = kWheelDiameter/2.0;
-    constexpr float kWheelCircumference = 2*M_PI*kWheelRaius;   
-    }
-
-    namespace PID
-    {
-    static constexpr float kKp = 4.8f;
-    static constexpr float kKi = 0.002f;
-    static constexpr float kKd = 0.06f;
-    static constexpr float kOmegaMax = 0.25f;
-    }
-
-
-}
+constexpr float kWheelDiameter = 0.109f;
+constexpr float kWheelRaius = kWheelDiameter/2.0;
+constexpr float kWheelCircumference = 2*M_PI*kWheelRaius;   
+} // namespace DriveConstants
+namespace PID
+{
+static constexpr float kKp = 4.8f;
+static constexpr float kKi = 0.002f;
+static constexpr float kKd = 0.06f;
+static constexpr float kOmegaMax = 0.25f;
+} // namespace PID
+} // namespace Constants
 
 #endif
 
