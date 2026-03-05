f9dfad737af10d3c49ecdf1f82faf39ac71d155a
Maximo2323 <a01384983@tec.mx>
2026-03-05 01:09:52 -0600

    refactor(muxtest): Implemented debugprint function and extra comments to explain function of test.

--- test/Regional_setup/muxtest.cpp
+++ test/Regional_setup/muxtest.cpp
@@ -3,16 +3,10 @@
 #include "mux.h"   // tu clase del 74HC4067
 #include "pins.h"
 
-// ---------------------------
-// Placeholders de pines MUX
-// ---------------------------
-static constexpr uint8_t PIN_MUX_SIG = Pins::kMuxSig;  // SIG (común) del mux -> pin analógico
-static constexpr uint8_t PIN_MUX_S0  = Pins::kMuxS0;  // S0
-static constexpr uint8_t PIN_MUX_S1  = Pins::kMuxS1;  // S1
-static constexpr uint8_t PIN_MUX_S2  = Pins::kMuxS2;  // S2
-static constexpr uint8_t PIN_MUX_S3  = Pins::kMuxS3;  // S3
+// Este code es para probar todos los canales del mux análogo
+// Da como resultado la señal analoga de cada uno de los canales
 
-Mux74HC4067 mux(PIN_MUX_SIG, PIN_MUX_S0, PIN_MUX_S1, PIN_MUX_S2, PIN_MUX_S3);
+Mux74HC4067 mux;
 
 void setup() {
   Serial.begin(115200);
@@ -26,14 +20,5 @@ void setup() {
 }
 
 void loop() {
-  for (uint8_t ch = 0; ch < 16; ch++) {
-    uint16_t v = mux.read(ch);
-    Serial.print(ch);
-    Serial.print(':');
-    Serial.print(v);
-    if (ch < 15) Serial.print('\t');
-  }
-  Serial.println();
-
-  delay(50); // ajusta si quieres más rápido/lento
+  mux.debugPrint();
 }
