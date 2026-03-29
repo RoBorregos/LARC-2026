7904137dbb250def40c19ac71e60769cb1c77af6
ximenapatriciagarcia <ximenascoding@gmail.com>
2026-03-29 03:31:21 -0600

    Modified : sorter class : Logic added

--- src/robot/sorter/sorter.cpp
+++ src/robot/sorter/sorter.cpp
@@ -4,11 +4,53 @@
  * @brief Source file for the sorter class.
  * @version 0.1
  * @date 2026-03-27
-*/
+ */
 
 #include "sorter.hpp"
 
-SORTER::SORTER()
+SORTER::SORTER() {}
+
+void SORTER::startStateTime()
+{
+    if (now == 0)
+    {
+        now = millis();
+    }
+}
+
+void SORTER::moveLeft()
+{
+    if(boxLeft){
+        
+    }
+}
+
+void SORTER::moveRight()
+{
+}
+
+void SORTER::separate(bool warmBean)
 {
 
+    if (warmBean)
+    {
+        separateS.write(90);
+    }
+    else
+    {
+        separateS.write(0);
+
+    }
+}
+
+void SORTER::box(SORTER box_, bool left)
+{
+    if (left)
+    {
+        box_.moveLeft();
+    }
+    else
+    {
+        box_.moveRight();
+    }
 }
--- src/robot/sorter/sorter.hpp
+++ src/robot/sorter/sorter.hpp
@@ -4,20 +4,38 @@
  * @brief Source file for the sorter class.
  * @version 0.1
  * @date 2026-03-27
-*/
+ */
 
 #ifndef SORTER_HPP
 #define SORTER_HPP
 
 #include <Arduino.h>
 #include "Servo.h"
+#include "pins.h"
+#include "constants.h"
+#include "robot/instances/instances.h"
 
 class SORTER
 {
-    public:
-        SORTER();
+public:
+    SORTER();
+    void separate(bool warmBean);     // warm or cold colored bean
+    void box(SORTER box_, bool left); // Left or right move of the box
 
-    private:
+private:
+    Servo separateS; //"RGB" (separates) servo
+    Servo boxS;      // Move box servo
+    Servo rightFree; // Box Right Servo
+    Servo leftFree;  // Box Left servo
+    bool boxLeft;
+    uint32_t now;
+
+    void moveLeft();
+    void moveRight();
+
+    // MILLIS
+    void startStateTime();
+    //const uint32_t now = millis();
 
 };
 
