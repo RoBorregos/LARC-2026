e28395b3d80aba6549f2b18eeca64c1ee30b3a52
ximenapatriciagarcia <ximenascoding@gmail.com>
2026-03-31 18:24:51 -0600

    Refactor setup function to initialize state machine and sensors

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,26 @@
 #include <Arduino.h>
-#include "pins.h"
+#include "robot/StateMachine/StateMachine.h"
+#include "robot/instances/instances.h"
+
+LARCStateMachine stateMachine;
 
 void setup()
 {
+    Serial.begin(115200);
+    while (!Serial && millis() < 3000) {}
 
+    LARC.begin();
+    ir.begin();
+    us1.begin();
+    us2.begin();
+    qtrFront.begin();
+    servos.begin();
+    elevator.begin();
+
+    stateMachine.begin();
 }
-
 
 void loop()
 {
-
+    stateMachine.update();
 }
