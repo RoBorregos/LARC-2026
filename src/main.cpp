66e4f26670e78a065fc350621a0052b2ea6ace22
ximenapatriciagarcia <ximenascoding@gmail.com>
2026-04-04 00:50:16 -0600

    Refactor main.cpp to remove Drive subsystem and integrate LARCStateMachine for streamlined control

--- src/main.cpp
+++ src/main.cpp
@@ -1,45 +1,26 @@
 #include <Arduino.h>
-#include "subsystem/Drive/Drive.hpp"
+#include "robot/StateMachine/StateMachine.h"
+#include "robot/instances/instances.h"
 
-Drive drive;
+LARCStateMachine stateMachine;
 
-static bool    started  = false;
-static uint32_t lastPrint = 0;
-
-void setup() {
+void setup()
+{
     Serial.begin(115200);
-    delay(500);
+    while (!Serial && millis() < 3000) {}
 
-    drive.begin();
-    drive.holdYaw(false);   // sin BNO, solo encoders
-    drive.resetOdometry();
+    LARC.begin();
+    ir.begin();
+    us1.begin();
+    us2.begin();
+    qtrFront.begin();
+    servos.begin();
+    elevator.begin();
 
-    Serial.println("=== TEST ODOMETRIA ===");
-    Serial.println("Formato: M1 M2 M3 M4 | OdoX OdoY");
+    stateMachine.begin();
 }
 
-void loop() {
-    drive.update();
-
-    // Arranca una sola vez
-    if (!started) {
-        started = true;
-        drive.forward(0.45f);
-    }
-
-    // Para a 0.5m
-    if (fabsf(drive.getOdoX()) >= 0.5f) {
-        drive.brake();
-    }
-
-    // Print 10 Hz
-    if (millis() - lastPrint >= 100) {
-        lastPrint = millis();
-        Serial.print(drive.getM1Meters(), 3); Serial.print("\t");
-        Serial.print(drive.getM2Meters(), 3); Serial.print("\t");
-        Serial.print(drive.getM3Meters(), 3); Serial.print("\t");
-        Serial.print(drive.getM4Meters(), 3); Serial.print("\t");
-        Serial.print(drive.getOdoX(), 3);     Serial.print("\t");
-        Serial.println(drive.getOdoY(), 3);
-    }
+void loop()
+{
+    stateMachine.update();
 }
