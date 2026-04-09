16ac78fd2c9b5b9306f5d473f2a6c12eb155f651
ximenapatriciagarcia <ximenascoding@gmail.com>
2026-04-09 17:21:22 -0600

    Add OdomMovement class for odometry and motor control integration

---
+++ lib/testOdometry/testOdometry.hpp
@@ -0,0 +1,140 @@
+#pragma once
+
+#include <Arduino.h>
+#include <Wire.h>
+#include <math.h>
+#include "pins.h"
+#include "BNO/bno.hpp"
+
+class OdomMovement
+{
+public:
+    OdomMovement();
+
+    void begin();
+    void update();
+
+    // movimientos de traslación
+    void forward(float rpm);
+    void backward(float rpm);
+    void right(float rpm);
+    void left(float rpm);
+
+    // detener
+    void stop();
+
+    // pose / odometría
+    void resetPose();
+    float getX() const;
+    float getY() const;
+    float getTheta() const;
+    float getThetaDeg() const;
+    float getDistance() const;
+
+    // progreso por eje
+    float getForwardProgress() const;   // fabs(x)
+    float getLateralProgress() const;   // fabs(y)
+
+    // yaw
+    float getYawNow() const;
+    void captureCurrentYawTarget();
+
+private:
+    static constexpr uint8_t FILTER_SIZE = 8;
+
+    static constexpr float kTs          = 0.05f;
+    static constexpr float kWheelRadius = 0.054f;
+    static constexpr float kInvSqrt2    = 0.70710678f;
+    static constexpr float kOdomScale   = 2.57f;
+    static constexpr float kPwmDeadband = 60.0f;
+    static constexpr float kPwmMax      = 120.0f;
+
+    static constexpr float kKp = 1.5f;
+    static constexpr float kKi = 1.2f;
+    static constexpr float kKd = 0.005f;
+
+    static constexpr float kYawKp  = 150.0f;
+    static constexpr float kYawKi  = 0.5f;
+    static constexpr float kYawKd  = 0.5f;
+    static constexpr float kYawMax = 30.0f;
+
+    struct Motor
+    {
+        uint8_t pwmPin, in1, in2;
+        volatile unsigned long period_buf[FILTER_SIZE];
+        volatile uint8_t period_idx;
+        volatile unsigned long last_pulse_us;
+        volatile bool got_pulse;
+        float PPR;
+        float Kp, Ki, Kd;
+        float setpoint;
+        float integral;
+        float last_error;
+        bool inverted;
+    };
+
+private:
+    static OdomMovement* instance_;
+
+    BNO bno_;
+
+    // pines
+    uint8_t encUL_A_, encUL_B_, pwmUL_, inUL1_, inUL2_;
+    uint8_t encUR_A_, encUR_B_, pwmUR_, inUR1_, inUR2_;
+    uint8_t encLL_A_, encLL_B_, pwmLL_, inLL1_, inLL2_;
+    uint8_t encLR_A_, encLR_B_, pwmLR_, inLR1_, inLR2_;
+
+    // motores
+    Motor UL_;
+    Motor UR_;
+    Motor LL_;
+    Motor LR_;
+
+    // LL especial por ticks
+    volatile long ticksLL_count_;
+    long prevTicksLL_;
+
+    // yaw PID
+    float yawTarget_;
+    float yawIntegral_;
+    float yawPrevErr_;
+    float yawNow_;
+
+    // EKF
+    float ekf_x_;
+    float ekf_y_;
+    float ekf_th_;
+    float P_[3][3];
+    float Q_[3][3];
+    float R_theta_;
+
+    // tiempo
+    uint32_t lastCycleMs_;
+
+private:
+    static float wrapPi(float a);
+    static void pushPeriod(Motor& m, unsigned long p);
+
+    float yawPidStep(float yawMeasured, float dt);
+
+    float measureRPM(Motor& m);
+    float measureRPM_LL(float dtSec);
+
+    void setMotorPWM(Motor& m, float pwm);
+    void stopMotor(Motor& m);
+    void stopAll();
+
+    void pidStepWithRPM(Motor& m, float rpm, float extraRPM);
+    void ekfStep(float dt, float rpmUL, float rpmUR, float rpmLL, float rpmLR);
+
+    void setRPMs(float ul, float ur, float ll, float lr);
+
+    // ISR
+    static void isrUL_A();
+    static void isrUL_B();
+    static void isrUR_A();
+    static void isrUR_B();
+    static void isrLR_A();
+    static void isrLR_B();
+    static void isrLL();
+};
